c3ce1b3fdb4ea40259533d13aab779495c83fb23
thoconxinhxan01 <01652581029vip@gmail.com>
2025-06-06 00:17:02 +0700

    Xây dựng giao diện lấy mã

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,18 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import 'expo-router/entry';
-import React from 'react';
 import App from './app';
 import LoginScreen from './screens/LoginScreen';
+import PasswordCodeScreen from './screens/PasswordCodeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack.Navigator initialRouteName="SignUp">
+    <Stack.Navigator initialRouteName="PasswordCode">
       <Stack.Screen name="app" component={App} options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp" component={RegisterScreen} options={{ headerShown: false }} />
+       <Stack.Screen name="PasswordCode" component={PasswordCodeScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
