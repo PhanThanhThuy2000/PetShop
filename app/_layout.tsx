66d5b2cc63e072cd1884f59a833c96c49d0e6103
ducnaph53199 <ducnaph53199@gmail.com>
2025-06-07 09:42:37 +0700

    PT-21 xây dựng giao diện cài đặt

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,17 +6,19 @@ import NewPasswordScreen from './screens/NewPasswordScreen';
 import PasswordCodeScreen from './screens/PasswordCodeScreen';
 import RecoveryPasswordScreen from './screens/RecoveryPasswordScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import SettingsScreen from './screens/SettingsScreen';
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="SettingsScreen">
       <Stack.Screen name="app" component={App} options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp" component={RegisterScreen} options={{ headerShown: false }} />
       <Stack.Screen name="PasswordCode" component={PasswordCodeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="RecoveryPasswordScreen" component={RecoveryPasswordScreen} options={{ headerShown: false }} />
       <Stack.Screen name="NewPassword" component={NewPasswordScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
