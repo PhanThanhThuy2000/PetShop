be7dfad32bce579f7a478fc236a9a4996c56c7b4
TriNguyenrk <anhtri8b@gmail.com>
2025-06-07 22:03:22 +0700

    PT-17 Xây dựng giao diện voucher

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,17 +6,19 @@ import NewPasswordScreen from './screens/NewPasswordScreen';
 import PasswordCodeScreen from './screens/PasswordCodeScreen';
 import RecoveryPasswordScreen from './screens/RecoveryPasswordScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import VoucherScreen from './screens/VoucherScreen';
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="VoucherScreen">
       <Stack.Screen name="app" component={App} options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp" component={RegisterScreen} options={{ headerShown: false }} />
       <Stack.Screen name="PasswordCode" component={PasswordCodeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="RecoveryPasswordScreen" component={RecoveryPasswordScreen} options={{ headerShown: false }} />
       <Stack.Screen name="NewPassword" component={NewPasswordScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="VoucherScreen" component={VoucherScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
