10d2e2fcb1b76e8b842b77c680b7ce31ab7fe6fe
PhanThanhThuy2000 <162803735+PhanThanhThuy2000@users.noreply.github.com>
2025-07-07 00:00:54 +0700

    PT-169 xem chi tiet san pham

--- app/utils/api-client.ts
+++ app/utils/api-client.ts
@@ -2,7 +2,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://172.20.10.7:5000/api';
+export const API_BASE_URL = 'http://192.168.0.101:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
